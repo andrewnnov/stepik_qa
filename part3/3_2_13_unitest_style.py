38712c17fceb43f3d6d457b54699aa264ced0e2d
andreybannikov <andrewnnov@yandex.ru>
2021-07-13 07:35:53 +0300

    use unittest style task 3_2_13

---
+++ part3/3_2_13_unitest_style.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+import time
+import unittest
+
+browser = webdriver.Chrome("C:\Projects\stepik_qa\driver\chromedriver.exe")
+
+
+class TestAbs(unittest.TestCase):
+
+    def test_successful_registration(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser.get(link)
+
+        input_first_name = browser.find_element_by_tag_name('[placeholder="Input your first name"]')
+        input_first_name.send_keys("Ivan")
+        input_last_name = browser.find_element_by_tag_name('[placeholder="Input your last name"]')
+        input_last_name.send_keys("Petrov")
+        input_email = browser.find_element_by_tag_name('[placeholder="Input your email"]')
+        input_email.send_keys("test@testmail.com")
+
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+        time.sleep(1)
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Texts Should be equal")
+
+
+    def test_unsuccessful_registration(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser.get(link)
+
+        input_first_name = browser.find_element_by_tag_name('[placeholder="Input your first name"]')
+        input_first_name.send_keys("Ivan")
+        input_last_name = browser.find_element_by_tag_name('[placeholder="Input your last name"]')
+        input_last_name.send_keys("Petrov")
+        input_email = browser.find_element_by_tag_name('[placeholder="Input your email"]')
+        input_email.send_keys("test@testmail.com")
+
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+        print(welcome_text)
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Texts Should be equal")
+
+    if __name__ == "__main__":
+        unittest.main()
+
+
+
+
