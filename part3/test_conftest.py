689dd383793aeb20e88ce89ff56ff6db263615bd
andreybannikov <andrewnnov@yandex.ru>
2021-07-18 22:34:46 +0300

    test examples

---
+++ part3/test_conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import math
+
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
